ec6d004eb76cf853808fdea2d61d63ca01b797ba
Kelvin Mai <kelvin.mai002@gmail.com>
2018-05-25 23:30:31 -0400

    Added poll chart

--- client/src/containers/Poll.jsx
+++ client/src/containers/Poll.jsx
@@ -1,7 +1,9 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
+import { Pie } from 'react-chartjs-2';
 
 import { vote } from '../store/actions';
+import { color } from '../services/color';
 
 const Poll = ({ poll, vote }) => {
   const answers =
@@ -14,12 +16,25 @@ const Poll = ({ poll, vote }) => {
       </button>
     ));
 
+  const data = {
+    labels: poll.options.map(option => option.option),
+    datasets: [
+      {
+        label: poll.question,
+        backgroundColor: poll.options.map(option => color()),
+        borderColor: '#323643',
+        data: poll.options.map(option => option.votes),
+      },
+    ],
+  };
+
   console.log('Poll Component: ', poll);
   console.log('Poll component: ', poll.options);
   return (
     <div>
       <h3>{poll.question}</h3>
       <Fragment>{answers}</Fragment>
+      <Pie data={data} />
     </div>
   );
 };
